b7785edaa3a08daf117cfa510973c2817efb1448
JIIN <quswldls3162@naver.com>
2026-01-19 11:46:15 +0900

    dd

--- fpga/src/memory/Frame_Buffer.sv
+++ fpga/src/memory/Frame_Buffer.sv
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 
 module Frame_Buffer #(
-    parameter RGB_WIDTH    = 24,
+    parameter DATA_WIDTH    = 24,
     parameter IMG_WIDTH    = 176,
     parameter IMG_HEIGHT   = 240,
     parameter TOTAL_PIXELS = IMG_WIDTH * IMG_HEIGHT,
@@ -11,20 +11,20 @@ module Frame_Buffer #(
     input  logic                  wclk,
     input  logic                  we,
     input  logic [ADDR_WIDTH-1:0] wAddr,
-    input  logic [RGB_WIDTH-1:0]  wData,
+    input  logic [DATA_WIDTH-1:0] wData,
     // Read port A
     input  logic                  rclk_a,
     input  logic                  oe_a,
     input  logic [ADDR_WIDTH-1:0] rAddr_a,
-    output logic [RGB_WIDTH-1:0]  rData_a,
+    output logic [DATA_WIDTH-1:0] rData_a,
     // Read port B
     input  logic                  rclk_b,
     input  logic                  oe_b,
     input  logic [ADDR_WIDTH-1:0] rAddr_b,
-    output logic [RGB_WIDTH-1:0]  rData_b
+    output logic [DATA_WIDTH-1:0] rData_b
 );
 
-    logic [RGB_WIDTH-1:0] mem[0:TOTAL_PIXELS-1];
+    logic [DATA_WIDTH-1:0] mem[0:TOTAL_PIXELS-1];
 
     // Write
     always_ff @(posedge wclk) begin
